1d47e111f484501e78ef80e2e5e35dfb47461978
borispantic <boris.pantic@outlook.fr>
2024-06-17 11:41:38 +0200

    put

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main( int argc, char * argv[] ) {
+
+	/* Fonction put pour ecrire le message demandé */
+
+	puts ( ""Programming is like building a multilingual puzzle" );
+
+return 0;
+
+}
