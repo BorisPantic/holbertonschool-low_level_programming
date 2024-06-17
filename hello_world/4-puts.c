0e49a726d46f485e41dc817edab9e861b9f1917d
borispantic <boris.pantic@outlook.fr>
2024-06-17 15:06:33 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints 'Programming is like building a multilingual puzzle'
+ * Return:0
+ */
+
 int main(void)
 {
 	/* Fonction puts pour ecrire le message de l'exercice */
-	puts("Programming is like building a multilingual puzzle)
-return0;
+	puts("\"Programming is like building a multilingual puzzle)
+return(0);
 }
