d6b6c4d7514d4756151222d442c370b191523781
borispantic <boris.pantic@outlook.fr>
2024-06-18 09:41:08 +0200

    print value of n var

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - prints if n is positive, negative or is equal to zero.
+ * Return:0
+ * */
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0 ){
+		printf("is positive");
+	}
+	else if(n > 0 ){
+		printf("is negative");
+	}
+	else(n = 0){
+		printf("is zero");
+	}
+	return (0);
+}
