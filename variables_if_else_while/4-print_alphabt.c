9376e01db5fa7e96e1f80a3e5466683092505579
borispantic <boris.pantic@outlook.fr>
2024-06-18 13:43:20 +0200

    print alphabet without e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Print alphabet exept q and e.
+ *Return:0
+ */
+
+int main(void)
+{
+char a = 'a';
+
+while (a <= 'z')
+if (a == 'q' && a == 'e')
+{}
+
+putchar(a++);
+
+putchar('\n');
+
+return (0);
+}
