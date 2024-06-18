7eaa4b2e033a050ea14ec2b61cc8458c6eb95177
borispantic <boris.pantic@outlook.fr>
2024-06-18 15:17:42 +0200

    last task

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - print 0 to 10 with , .
+ *Return:0 
+ */
+
+int main(void)
+{
+char i = '0';
+char u =',';
+while (i <= '10')
+putchar(i++,u);
+putchar('\n);
+return (0);
+}
