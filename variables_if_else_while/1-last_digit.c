6e6d6591d84f915e3dba6452207da249a7823952
borispantic <boris.pantic@outlook.fr>
2024-06-18 10:59:16 +0200

    determine the value of the last digit of n

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - prints last digit of the n var.
+ * return 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is", n);
+	n %= 10;
+	if (n > 5)
+		printf(" %d and is greater than 5\n", n);
+	if (n == 0)
+		printf(" %d and is 0\n", n);
+	if (n < 6 && != 0)
+		printf(" %d and is less than 6 and not 0\n", n);
+	return (0);
+}
