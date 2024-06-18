e15a2c3821e90c0da1050d664bfd392c929870ef
borispantic <boris.pantic@outlook.fr>
2024-06-18 14:09:17 +0200

    more putchar

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints all the number 1 to 10 followed by a new line.
+ *Return:0
+ */
+
+int main(void)
+{
+char a = '1';
+
+while (a <= '10')
+
+putchar(a++);
+
+putchar('\n');
+
+return (0);
+}
