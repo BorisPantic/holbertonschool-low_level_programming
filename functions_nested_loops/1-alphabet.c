d7a49c8fe89eab759418590f5e97336fb20b9385
borispantic <boris.pantic@outlook.fr>
2024-06-19 11:28:19 +0200

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - ta grand mere
+ * Return:0
+ */
+
+int main(void)
+{
+char a = 'a';
+while ('c' <= 'z')
+_putchar(a++);
+_putchar('\n');
+return (0);
+}
