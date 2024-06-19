0bf31d5be62347106cf7eaa8c85a5a27e1c912bf
borispantic <boris.pantic@outlook.fr>
2024-06-19 14:50:39 +0200

    function who print alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - fuction who print alphabet 10 times .
+ */
+
+void print_alphabet_x10(void)i
+{
+for (int i = 0; i <= 10 ; i++)
+{
+print_alphabet();
+}
+}
