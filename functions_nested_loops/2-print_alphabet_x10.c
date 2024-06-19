9ddca4503d3a752265c0229158947c73b06d365c
borispantic <boris.pantic@outlook.fr>
2024-06-19 15:57:57 +0200

    function who print alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,10 +4,15 @@
 /**
  *print_alphabet_x10 - fuction who print alphabet 10 times .
  */
-void print_alphabet(void);
+
 void print_alphabet_x10(void)
-{
-int i = 1;
-while (i <= 10)
-print_alphabet();
+{	
+	char b = 0
+	while (b <= 10 ;b++)
+	{
+		char c = 'a';
+		while (c <= 'z')
+		_putchar(c++);
+		_putchar('\n');
+		}
 }
