dd7db0b368ee1c8150a04994b63e1254ceaa82bc
borispantic <boris.pantic@outlook.fr>
2024-06-19 14:28:02 +0200

    create printalphabet function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *void - function for print alphabet stocked in main.h .
+ *print_alphabet - function for print alphabet stocked in main.h .
  *Return:0
  */
 
-void print_alphabet(void) 
+void print_alphabet(void)
 {
 char a = 'a';
 while (a <= 'z')
