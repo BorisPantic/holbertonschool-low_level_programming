a4977fb64bf5ad3f283644111bf4173957d5f83a
borispantic <boris.pantic@outlook.fr>
2024-06-20 11:55:17 +0200

    Write a function that checks for alphabetic character.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- *  _isalpha - check if the caracter is UPPER or LOWER case then print 1 if not print 0
+ *  _isalpha - 
+ *  check if the caracter is UPPER or LOWER case then print 1 if not print 0
  *  Return: 1 if lower or upper case 0if else
  *  @c: The caracter check
  */
