a70a1fe73dcb1231115106eb8afa42f565b873e6
borispantic <boris.pantic@outlook.fr>
2024-06-20 13:45:48 +0200

    (Write a function that prints the last digit of a number.

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number.
+ * Return:r
+ * @r: number
+ */
+
+int print_last_digit(int r)
+{
+	for (r % 10)
+		{
+		return (r);
+		}
+}	
