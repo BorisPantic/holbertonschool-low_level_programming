120c56c1bbf5d8262ec8de6dac98a1b3327b3010
borispantic <boris.pantic@outlook.fr>
2024-06-20 14:30:17 +0200

    addition

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,11 +2,15 @@
 #include "main.h"
 
 /**
- *
+ * add - sum a + b
+ * Return:sum
+ * @sum = a+b
+ * @num1: a
+ * @num2: b
  */
 
 int add(int num1, int num2)
 {
-  int sum = num1 + num2;
-  return (sum);
+int sum = num1 + num2;
+return (sum);
 }
