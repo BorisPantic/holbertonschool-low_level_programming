3cc770e7a9c2e93ed0e3797de7b0a382d8f73120
borispantic <boris.pantic@outlook.fr>
2024-06-21 11:07:05 +0200

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,20 +6,15 @@
  */
 void times_table(void)
 {
-    int i = 9;
-    int j = 0;
-    int z;
-    i = 9;
-    do (j + 1; j++)
-    while (j == 9)
-        {
-	    z = i * j;
-            _putchar(z);
-            if (j < 9)
-            {
-                _putchar(',');
+int i = 0;
+int j = 9;
+int z;
+	while (i <= 9;i++);
+		{
+		z = i * j;
+		_putchar(z);
+		while (j < 9)
+		_putchar(',');
 		_putchar(' ');
-            }
-        }
-       _putchar('\n');
+		}
 }
