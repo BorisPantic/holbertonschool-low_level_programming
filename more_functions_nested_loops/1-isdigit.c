81271454f9eda1365cb07358ce0a5a08e2f2355e
borispantic <boris.pantic@outlook.fr>
2024-06-24 11:21:01 +0200

    check if digit or not

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- *
+ *_isdigit - check if it's a number or else.
+ *@c: random value to check
+ *Return: 1 if it's a number and 0 for else value.
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
