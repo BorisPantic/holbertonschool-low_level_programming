483b4df9c8e5f68bfcd2763de2021d173825ac40
borispantic <boris.pantic@outlook.fr>
2024-06-24 14:19:00 +0200

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int z;
+
+	for (z = 0; z < n;z++)
+	{
+		_putchar(' ');
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\');
+	_putchar('\n');
+}
