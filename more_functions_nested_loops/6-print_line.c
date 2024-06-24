9909bbcbe00f3275d2993e403ef625ac9908d7e9
borispantic <boris.pantic@outlook.fr>
2024-06-24 13:53:04 +0200

    function that print lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- *
+ *print_line - print a line with the value of n.
+ *@n: value that determine the lenght of _ line.
  */
 
 void print_line(int n)
 {
-	int z;
+int z;
 
-        for (z = 0; z < n; z++)
-        {
-                _putchar('_');
-        
-        	if (n <= 0)
-        	{
-                _putchar('\n');
-       		}
+	for (z = 0; z < n; z++)
+	{
+		_putchar('_');
+		if (n <= 0)
+		{
+		_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
