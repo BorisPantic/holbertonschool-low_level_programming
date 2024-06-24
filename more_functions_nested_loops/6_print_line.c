f767e7f5c37685dab06864df223e7fbaf9a01e93
borispantic <boris.pantic@outlook.fr>
2024-06-24 13:43:27 +0200

    function that print lines

---
+++ more_functions_nested_loops/6_print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void print_line(int n)
+{
+        if (n >= 0)
+        {
+                _putchar(n * '_');
+                _putchar('\n');
+        }
+        if (n <= 0)
+        {
+                _putchar('\n');
+        }
+}
