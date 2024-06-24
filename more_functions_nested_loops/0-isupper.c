635e6753d030c1bc075d62f48032fc9d51eaf9a0
borispantic <boris.pantic@outlook.fr>
2024-06-24 10:09:10 +0200

    isupper function

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,11 @@
 int _isupper(int c)
 
 {
-	if (c = 'A'; c <= 'Z')
+	if (c = 'A'; c <= 'Z'; c++)
 	{
 		return(1);
 	}
-	else (c = 'a'; c <= 'z')
+	else (c = 'a'; c <= 'z'; c++)
 	{
 		return(0);
 	}
