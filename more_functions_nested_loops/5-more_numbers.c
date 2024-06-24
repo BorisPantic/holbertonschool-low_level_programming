3cc1120f752b87915b3601f798a4cc8ee61f246d
borispantic <boris.pantic@outlook.fr>
2024-06-24 13:20:13 +0200

    function that print numbers 10 times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ int numbers;
 
 	for (dixfois = '0'; dixfois < '9'; dixfois++)
 	{
-		for (numbers = '0'; numbers <= 14; numbers++)
-			_putchar(numbers);
-			_putchar('\n');
+	for (numbers = '0'; numbers <= 14; numbers++)
+		_putchar(numbers);
+	_putchar('\n');
 	}
 }
