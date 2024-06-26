3e70849c8624f6e4cf02937aa36f1e46d386a2a7
borispantic <boris.pantic@outlook.fr>
2024-06-26 14:31:06 +0200

    function that print string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- *
+ *print_rev - function that print string in reverse
+ *@s: spring
  */
 
 void print_rev(char *s)
 {
-	int i =0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
