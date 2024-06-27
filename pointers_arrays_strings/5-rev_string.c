7b488c7225248ae4a438f7f435b22badf4e38d79
borispantic <boris.pantic@outlook.fr>
2024-06-27 10:42:40 +0200

    function taht reverse a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,26 +9,24 @@ void rev_string(char *s)
 {
 	
 	int lenght = 0;
+	int start = 0;
+	int end = lenght - 1;
+	char temp;
 
 	while (*s != '\0')
 		{
 		lenght++;
 		s++;
 		}
-
-	int start = 0;
-	int end = lenght - 1;
-	char temp;
-
 	while (start < end)
-	{
+		{
 		temp = s[start];
 		s[start] = s[end];
 		s[end] = temp;
 
 		start++;
 		end--;
-	}
+		}
 	_putchar('\n');
 }
 
