b3deb03221f9cd6cf887567b07b82a135482575a
borispantic <boris.pantic@outlook.fr>
2024-06-28 12:26:00 +0200

    strncat function

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *de = dest;
+	while (*de != '\0')
+	{
+		de++;
+	}
+
+	while (n > 0 && *src != '\0')
+	{
+		*de = *src;
+		de++;
+		src++;
+		n--;
+	}
+	*d = '\0';
+	return (dest);
+}
