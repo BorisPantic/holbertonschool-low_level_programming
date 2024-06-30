0d906766099e267dc31cf47f33119929bbfb4dbd
borispantic <boris.pantic@outlook.fr>
2024-06-30 19:47:48 +0200

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ *_strncpy- function that copies a string
+ *@dest: destination
+ *@src: source
+ *@n: n
+ *Return:dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (; index < n ; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}
