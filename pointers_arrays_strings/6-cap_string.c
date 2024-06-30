8e011a1d5f6081365f69183b1d6a8cc8927353c0
borispantic <boris.pantic@outlook.fr>
2024-06-30 19:52:49 +0200

    cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *cap_string- function that capitalizes all words of a string
+ *@p: pointer of string
+ *Return: p
+ */
+
+char *cap_string(char *p)
+{
+	int index = 0;
+
+	while (p[index] != '\0')
+	{
+		if ((index == 0 || p[index - 1] == ' ' || p[index - 1] == '\t' ||
+		p[index - 1] == '.' || p[index - 1] == '\n')
+		&& p[index] >= 'a' && p[index] <= 'z')
+
+		p[index] -= 32;
+		index++;
+	}
+	return (p);
+}
