5596245e31dc80d8c0c44c8ad3d6f2d20046cee6
borispantic <boris.pantic@outlook.fr>
2024-07-02 14:43:14 +0200

    task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memcpy - copy memory
+ *@dest: destination str
+ *@src: source str
+ *@n: bytes to copy
+ *Return: destination str
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = *dest;
+	}
+	return (dest);
+}
