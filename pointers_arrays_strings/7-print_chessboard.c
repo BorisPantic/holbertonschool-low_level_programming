47f016b78ddba37b11cf7750775201c2655ce69a
borispantic <boris.pantic@outlook.fr>
2024-07-02 14:44:45 +0200

    task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a chess board
+ * @a: array of the chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int largeur;
+	int hauteur;
+
+	for (largeur = 0; largeur < 8; largeur++)
+	{
+		for (hauteur = 0; hauteur < 8; hauteur++)
+		
+			_putchar(a[largeur][hauteur]);
+		_putchar('\n');
+	}
+}
+	
+
