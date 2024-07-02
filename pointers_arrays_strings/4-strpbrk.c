7589da8119786da203714525b0604ab78804eb5a
borispantic <boris.pantic@outlook.fr>
2024-07-02 14:44:20 +0200

    task3

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strpbrk - search a string for a set of bytes
+ *@s: pointer to the string
+ *@accept : accept string
+ *Return:0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+	
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			{
+				return (s);
+			}
+		}
+		++s;
+	}
+	return (0);
+}
+			
