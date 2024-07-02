e033012f58f68dfee99ff5ce42ee8c687b59551f
borispantic <boris.pantic@outlook.fr>
2024-07-02 14:23:23 +0200

    x power y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - x power y 
+ * _pow_recursion - x power y
  * @x: value
  * @y: power
  * Return: x factorial y
@@ -10,10 +10,10 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	return (-1);
+		return (-1);
 	else if (y == 0)
-	return (1);
+		return (1);
 	else
-	return (x * _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 
 }
