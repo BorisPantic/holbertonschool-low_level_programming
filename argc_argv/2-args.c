05146533c3a9000abaf517f07c258f768c7904be
borispantic <boris.pantic@outlook.fr>
2024-07-03 16:53:44 +0200

    argv multiple argument

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all argument string with argv
+ * @argc: void
+ * @argv: argument to print
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7]);
+	return (0);
+}
