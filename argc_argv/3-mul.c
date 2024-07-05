9c10d7f83ad8959f3057b6aef69912b3f3e2c9e2
borispantic <boris.pantic@outlook.fr>
2024-07-05 15:41:31 +0200

    multiplication

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,17 +11,18 @@
 int main(int argc, char **argv)
 {
 	int v1 = 0, v2 = 0, r = 0;
-        if (argc != 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        if (argc == 3)
-        {
-                v1 = atoi(argv[1]);
-                v2 = atoi(argv[2]);
-                r = (v1 * v2);
-                printf("%d\n", r);
-        }
-        return (0);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc == 3)
+	{
+		v1 = atoi(argv[1]);
+		v2 = atoi(argv[2]);
+		r = (v1 * v2);
+		printf("%d\n", r);
+	}
+	return (0);
 }
