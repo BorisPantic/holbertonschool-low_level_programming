9dbffb6af1755a999ff562754bf0722100455ab4
borispantic <boris.pantic@outlook.fr>
2024-07-11 11:40:53 +0200

    dog header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog.h - dog header 
+ * @age: dog age
+ * @owner: owner of the dog
+ * @age: age of dog
+ */
+
+struct my_struct
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
