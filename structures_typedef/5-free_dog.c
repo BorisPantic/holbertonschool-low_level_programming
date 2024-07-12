93a27be9ec9d8c1f124fb31f89849283de8f086b
borispantic <boris.pantic@outlook.fr>
2024-07-12 13:51:23 +0200

    new dog + free memory

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *free_dog - function that unallocate memory of d , owner and age.
+ *@d: pointer to dog owner age and name value
+ */
+
+void free_dog(dog_t *d);
+{
+	if (d != NULL)
+	{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
